59ccb3420412a14923dffa995c3bd4c810c43a61
Ubuntu <ubuntu@ip-172-31-42-169.ap-southeast-1.compute.internal>
2024-02-09 02:41:24 +0000

    adding data generator

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-cmake -B ../apsi2 -S . -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake -DAPSI_BUILD_CLI=ON
-cmake --build ../apsi2
+cmake -B ../v2 -S . -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake -DAPSI_BUILD_CLI=ON
+cmake --build ../v2
---
+++ cli/data/data_generator.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+    cout << "Number of bits: " << stoi(argv[1]) + 7 << "\n";
+}
